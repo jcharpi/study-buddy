643841dca76228a920627cc0ec60ab62af1865e0
Josh Charpentier <charpentier.josh@gmail.com>
2023-05-22 15:00:37 -0500

    removed log

--- pages/TaskActivePage.tsx
+++ pages/TaskActivePage.tsx
@@ -80,7 +80,6 @@ export default function TaskActivePage({ navigation }: any) {
 
 	function handleTick() {
 		secondsElapsedCount++
-		console.log(secondsElapsedCount)
 	}
 
 	const getTaskProgress = useCallback(
